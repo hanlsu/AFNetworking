ca8670acb149f704e7c3ea635dc2202989222eea
hanlsu <hl@HL.local>
2017-03-23 23:01:28 +0800

    add a myString property

--- AFNetworking/AFHTTPSessionManager.h
+++ AFNetworking/AFHTTPSessionManager.h
@@ -75,6 +75,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface AFHTTPSessionManager : AFURLSessionManager <NSSecureCoding, NSCopying>
 
+@property (nonatomic, copy) NSString *myString;
+
+
+
 /**
  The URL used to construct requests from relative paths in methods like `requestWithMethod:URLString:parameters:`, and the `GET` / `POST` / et al. convenience methods.
  */
